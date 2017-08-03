fff7dad90d5f8587267d06da6a1d729c683318d4
Hiswe <hiswehalya@gmail.com>
2017-08-03 18:39:09 +0700

    PostgreSQL – clean mongoDB old error handling

--- server/models/index.js
+++ server/models/index.js
@@ -13,42 +13,6 @@ const Gallery     = require( './model-gallery' )
 const ImageCache  = require( './model-image-cache' )
 
 //////
-// ERRORS HANDLING
-//////
-
-// normalize errors between mongoose & mongoDB
-function handleValidationErrors(err) {
-  console.log('handleValidationErrors')
-  console.log(util.inspect(err))
-  // mongoose errors
-  if (err.name === 'ValidationError') {
-    return Promise.resolve(err.errors)
-  }
-  // duplicated field
-  if (err.name === 'MongoError' && err.code === 11000) {
-    // mongo doesn't provide field name out of the box
-    // fix that based on the error message
-    var fieldName = /index:\s([a-z]*)/.exec(err.message)[1]
-    var errorMsg  = {}
-    errorMsg[fieldName] = {
-      message: `this ${fieldName} is already taken`,
-    }
-    return Promise.resolve(errorMsg)
-  }
-  return Promise.reject(err)
-}
-
-// take care of everything
-function formatErrors(err, req, res, next) {
-  handleValidationErrors(err)
-  .then( errorMessages => {
-    req.flash('error', errorMessages)
-    res.redirect(req.path)
-  })
-  .catch(next)
-}
-
-//////
 // HELPERS
 //////
 
@@ -106,8 +70,7 @@ Gallery.belongsTo( Template )
 
 module.exports    = {
   sequelize,
-  // utilities,
-  formatErrors,
+  // helpers
   addGroupFilter,
   addStrictGroupFilter,
   // Models
