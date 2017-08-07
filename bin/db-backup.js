13e58883eb9febeaa2e58c95160b1af86b176493
Hiswe <hiswehalya@gmail.com>
2017-08-07 16:19:47 +0200

    DB – fix script to backup

--- bin/db-backup.js
+++ bin/db-backup.js
@@ -1,17 +1,22 @@
 'use strict'
 
-var exec          = require('child_process').exec
-var c             = require('chalk')
-var moment        = require('moment')
-var inquirer      = require('inquirer')
+const exec      = require( 'child_process' ).exec
+const c         = require( 'chalk' )
+const moment    = require( 'moment' )
+const inquirer  = require( 'inquirer' )
+const path      = require( 'path' )
+const fs        = require( 'fs-extra' )
+const pg        = require( 'pg' )
+const copyTo    = require( 'pg-copy-streams' ).to
 
-var config        = require('../server/config')
-var db            = config.dbConfigs
-var now           = moment().format('YYYY-MM-DD_HH:mm')
-var u             = require('./_db-utils')
-var tmpFolder, dbFrom, sourceName
+const config      = require( '../server/config' )
+const db          = config.dbConfigs
+const now         = moment().format( 'YYYY-MM-DD_HH-mm' )
+const h           = require( '../server/helpers' )
+const u           = require( './_db-utils' )
+const { tables }  = u
 
-let selectDb      = inquirer.prompt([
+const selectDb    = inquirer.prompt([
   {
     type:     'list',
     name:     'source',
@@ -20,25 +25,48 @@ let selectDb      = inquirer.prompt([
   },
 ])
 
-Promise
-.all([selectDb, config.setup])
-.then( (results) => {
-  let promptConf  = results[0]
-  let conf        = results[1]
-  sourceName      = promptConf.source
-  dbFrom          = db[sourceName]
-  tmpFolder       = conf.images.tmpDir
-  let dumpCmd   = `mongodump ${u.setDbParams(dbFrom)} -o ${tmpFolder} --excludeCollectionsWithPrefix objectlabs`
-  console.log(c.blue('Backing up'), sourceName, c.gray(dumpCmd))
-  exec(dumpCmd, onDbDump)
-})
-.catch(console.log)
+async function start() {
+  const [ promptConf, conf ] = await Promise.all( [selectDb, config.setup] )
+  const sourceName  = promptConf.source
+  const dbFrom      = db[ sourceName ]
+  const tmpFolder   = conf.images.tmpDir
+  const client      = new pg.Client( { connectionString: dbFrom })
+  const folderPath  = path.join(tmpFolder, `/backup-${sourceName}_${now}/` )
 
-function onDbDump(error, stdout, stderr) {
-  if (error !== null) return u.logErrorAndExit(error, `error in dumping ${sourceName}`)
-  let backupName  = `${tmpFolder}/backup-${sourceName}-${now}`
-  exec(`mv ${tmpFolder}/${dbFrom.folder} ${backupName}`, () => {
-    console.log(c.green('backing up done'), c.grey('to'), backupName)
-    process.exit(0)
-  })
+  console.log( c.blue('Backing up'), sourceName, dbFrom )
+  await Promise.all([
+    client.connect(),
+    fs.ensureDir( folderPath ),
+  ])
+
+  function copy( tableName ) {
+    const filePath  = path.join(folderPath, `${tableName}.csv` )
+    const deferred  = h.defer()
+    // quotes are needed to force respecting case
+    const query     = `COPY "${tableName}" TO STDOUT CSV HEADER`
+    // const query     = `COPY ${tableName} TO STDOUT WITH (FORMAT CSV, HEADER, QUOTE '"', FORCE_QUOTE *)`
+    fs.ensureFileSync( filePath )
+    const writeStream   = fs.createWriteStream(filePath)
+    const stream        = client.query( copyTo(query) )
+    stream
+    .pipe( writeStream )
+    .on( 'error', deferred.reject )
+    // wait for dest files to finish to be writen
+    // https://github.com/brianc/node-pg-copy-streams/issues/61#issuecomment-277010285
+    writeStream
+    .on( 'finish', deferred.resolve )
+    return deferred
+  }
+
+  try {
+    await Promise.all( tables.map(copy) )
+  } catch(e) {
+    console.log( e )
+    return process.exit( 1 )
+  }
+  await client.end()
+  console.log( c.green('backing up done'), c.grey('to'), folderPath )
+  process.exit( 0 )
 }
+
+start()
