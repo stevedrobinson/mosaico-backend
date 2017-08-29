7eb9b2b41211afb00c993e61372d12316b19424e
Hiswe <hiswehalya@gmail.com>
2017-08-29 17:44:12 +0200

    TESTS – add tags filter

--- tests/functional/mailing-filter.js
+++ tests/functional/mailing-filter.js
@@ -29,6 +29,20 @@ const updateFilter = nightmare => {
   return nightmare
     .realClick( `.js-filter button[type=submit]`  )
     .wait( WAIT )
+    .realClick( `.js-toggle-filter` )
+    .wait( WAIT )
+}
+
+// Has to do this in order to have a multiple selection…
+const selectMultiple = (selectQuery, value) => {
+  const select = document.querySelector( selectQuery )
+  if (!select) return false
+  let option = select.querySelector( `option[value="${value}"]` )
+  if (!option) return false
+  option.selected = true
+  const changeEvent = new Event('change')
+  select.dispatchEvent( new Event('change') )
+  return true
 }
 
 //////
@@ -100,20 +114,8 @@ test( T2, createTest( 4, false, async (t, nm, close) => {
 
   t.ok( initialState.count > 1, `${T2} – we have more than one template to begin with` )
 
-  // Has to do this in order to have a multiple selection…
   const setSelection = await nm
-    .evaluate( templateId => {
-      const select = document.querySelector( `#template-field` )
-      if (!select) return false
-
-      let option = select.querySelector( `option[value="${templateId}"]` )
-      if (!option) return false
-      option.selected = true
-
-      const changeEvent = new Event('change')
-      select.dispatchEvent( new Event('change') )
-      return true
-    }, data.VERSAFIX_ID )
+    .evaluate( selectMultiple, `#template-field`, data.VERSAFIX_ID )
 
   if ( !setSelection ) return t.end( `can't select a template `)
 
@@ -176,3 +178,67 @@ test( T3, createTest( 4, false, async (t, nm, close) => {
 // TAGS
 //////
 
+const T4 = 'mailing – filter by tags'
+test.only( T4, createTest( 13, false, async (t, nm, close) => {
+  await serverReady
+  await resetDB()
+
+  const TAGS = ['silver', 'copper', 'gold']
+
+  const getTags = TAGS => {
+    const mailings    = document.querySelectorAll( `tbody tr td:nth-child(5)` )
+    const tagsContent = [...mailings].map( m => m.textContent )
+    const result      = { noTag: 0 }
+    TAGS.forEach( t => result[ t ] = 0 )
+    tagsContent.forEach( tagText => {
+      tagText = tagText.trim()
+      if (!tagText) return result.noTag += 1
+      TAGS.forEach( t => tagText.indexOf( t ) >= 0 ? result[ t ] += 1 : void 0 )
+    })
+    return Object.freeze( result )
+  }
+
+  const initialState = await nm
+    .use( connectUser() )
+    .use( allMailings )
+    .evaluate( getTags, TAGS )
+
+  t.ok( initialState.noTag > 0, `${T4} – we have mailings without tags` )
+
+  const t1 = await nm
+    .evaluate( selectMultiple, `#tag-field`, TAGS[2] )
+    .wait( WAIT )
+    .use( updateFilter )
+    .evaluate( getTags, TAGS )
+
+  t.equal( t1.noTag, 0, `${T4} – mailings without tags are skipped` )
+  t.equal( initialState[TAGS[2]], t1[TAGS[2]], `${T4} – third tag is preserved` )
+  t.notEqual( initialState[TAGS[1]], t1[TAGS[1]], `${T4} – second tag is skipped` )
+  t.notEqual( initialState[TAGS[0]], t1[TAGS[0]], `${T4} – first tag is skipped` )
+
+  const t2 = await nm
+    .evaluate( selectMultiple, `#tag-field`, TAGS[1] )
+    .wait( WAIT )
+    .use( updateFilter )
+    .evaluate( getTags, TAGS )
+
+  t.equal( t2.noTag, 0, `${T4} – mailings without tags are skipped` )
+  t.equal( initialState[TAGS[2]], t2[TAGS[2]], `${T4} – third tag is preserved` )
+  t.equal( initialState[TAGS[1]], t2[TAGS[1]], `${T4} – second selected tag is preserved` )
+  t.notEqual( initialState[TAGS[0]], t2[TAGS[0]], `${T4} – first tag is still skipped` )
+
+  const t3 = await nm
+    .evaluate( selectMultiple, `#tag-field`, TAGS[0] )
+    .wait( WAIT )
+    .use( updateFilter )
+    .evaluate( getTags, TAGS )
+
+  t.equal( t3.noTag, 0, `${T4} – mailings without tags are skipped` )
+  t.equal( initialState[TAGS[2]], t3[TAGS[2]], `${T4} – third tag is preserved` )
+  t.equal( initialState[TAGS[1]], t3[TAGS[1]], `${T4} – second selected tag is preserved` )
+
+  await close()
+
+  t.equal( initialState[TAGS[0]], t3[TAGS[0]], `${T4} – first selected is preserved` )
+
+}))
