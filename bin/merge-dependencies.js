5c8d0f1dd77306357fc2b9d0550c21b36110ea57
Hiswe <hiswehalya@gmail.com>
2017-09-15 11:48:45 +0200

    HEROKU – fix merge dependencies script

--- bin/merge-dependencies.js
+++ bin/merge-dependencies.js
@@ -1,12 +1,15 @@
 'use strict'
 
-const fs            = require( 'fs' )
-const path          = require( 'path' )
+const fs                  = require( 'fs' )
+const path                = require( 'path' )
 
-const basePackage   = require( '../package.json' )
-const dependencies  = { dependencies: basePackage.devDependencies }
-const mergedPackage = Object.assign( {}, basePackage, dependencies )
-delete mergedPackage.devDependencies
-const filepath      = path.join( __dirname, '../package.json' )
+const basePackage         = require( '../package.json' )
+const {
+  dependencies,
+  devDependencies,
+}                         = basePackage
+basePackage.dependencies  = Object.assign( {}, dependencies, devDependencies )
+delete basePackage.devDependencies
+const filepath            = path.join( __dirname, '../package.json' )
 
-fs.writeFileSync( filepath , JSON.stringify(mergedPackage, null, 2) )
+fs.writeFileSync( filepath , JSON.stringify(basePackage, null, 2) )
